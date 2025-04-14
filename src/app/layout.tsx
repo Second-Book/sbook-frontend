bb08dcf9d4cbcaeac19b58a8b0782b755ceb2264
SentFrom <sentfrom8@gmail.com>
2025-04-14 04:13:36 +0200

    redesigned nav bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,43 @@
-import type { Metadata, Viewport } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import UserStoreProvider from "@/providers/UserStoreProvider";
-import Navbar from "@/components/Navbar";
+import type { Metadata, Viewport } from "next"
+import { Poppins, Roboto } from "next/font/google"
+import "./globals.css"
+import UserStoreProvider from "@/providers/UserStoreProvider"
+import Navbar from "@/components/Navbar"
 
-const inter = Inter({
-  variable: "--font-inter",
-  subsets: ["latin"],
-});
+const roboto = Roboto({
+	variable: "--font-roboto",
+	subsets: ["latin"],
+})
+
+const poppins = Poppins({
+	variable: "--font-poppins",
+	subsets: ["latin"],
+	weight: ["300"],
+})
 
 export const metadata: Metadata = {
-  title: "EduMarket",
-  description: "Your place for used books",
-};
+	title: "EduMarket",
+	description: "Your place for used books",
+}
 
 export const viewport: Viewport = {
-  width: "device-width",
-  initialScale: 0
+	width: "device-width",
+	initialScale: 0,
 }
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode
 }>) {
-  return (
-    <html lang="en">
-      <body className={`${inter.variable}`}>
-        <UserStoreProvider>
-          <Navbar />
-          {children}
-        </UserStoreProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={`${roboto.variable} ${poppins.variable}`}>
+				<UserStoreProvider>
+					<Navbar />
+					{children}
+				</UserStoreProvider>
+			</body>
+		</html>
+	)
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,77 @@
 "use client"
 
-import useUserStore from '@/hooks/useUserStore'
-import Link from 'next/link'
+import useUserStore from "@/hooks/useUserStore"
+import {
+	faBookOpenReader,
+	faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import Link from "next/link"
+import Form from "next/form"
 
 const Navbar = () => {
-    const { isAuthenticated, logout } = useUserStore((state) => state)
+	const { isAuthenticated, logout } = useUserStore((state) => state)
 
-    return (
-        <header className="flex sticky top-0 z-2 justify-between items-center w-full text-white bg-rose-800">
-            <div className="flex items-center justify-start">
-                <Link href="/textbooks" className="text-xl sm:text-2xl font-bold px-4 leading-none">EduMarket</Link>
-            </div>
-            <nav className="flex text-lg sm:text-xl leading-tight justify-center">
-                {!isAuthenticated && <Link href="/login" className="focus:outline-none px-5 py-3 focus:ring-2 focus:ring-white hover:bg-rose-900">Sign In</Link>}
-                {!isAuthenticated && <Link href="/signup" className="focus:outline-none px-5 py-3 focus:ring-2 focus:ring-white hover:bg-rose-900">Sign Up</Link>}
-                {isAuthenticated && <Link href="/profile" className="focus:outline-none px-5 py-3 focus:ring-2 focus:ring-white hover:bg-rose-900">Profile</Link>}
-                {isAuthenticated && <button onClick={() => logout()} className="focus:outline-none px-5 py-3 focus:ring-2 focus:ring-white cursor-pointer hover:bg-rose-900">Logout</button>}
-            </nav>
-        </header>
-    )
+	return (
+		<header className="flex sticky top-0 z-2 items-center w-full h-24 text-xl text-black bg-white px-20 border-b gap-6 border-b-[#DDE1E6]">
+			<Link
+				href="/textbooks"
+				className="sm:text-2xl font-bold leading-none text-[#697077] text-nowrap">
+				<FontAwesomeIcon
+					icon={faBookOpenReader}
+					className="text-rose-800 mr-1"
+				/>
+				Second Book
+			</Link>
+			<Form action="/textbooks" className="grow">
+				<div className="w-full h-12 flex items-center bg-[#F2F4F8] rounded-xl border-b border-b-[#C1C7CD] text-lg pl-4 gap-2 focus-within:outline">
+					<FontAwesomeIcon
+						icon={faMagnifyingGlass}
+						className="text-[#697077]"
+					/>
+					<input
+						type="text"
+						name="query"
+						placeholder="Search"
+						className="w-full h-full focus:outline-none placeholder:text-black"
+					/>
+				</div>
+			</Form>
+			{!isAuthenticated && (
+				<Link
+					href="/login"
+					className="focus:outline-none focus:ring-2 focus:ring-white font-medium relative after:absolute [&:not(:hover)]:after:hidden after:top-11/10 after:left-0 after:w-full after:h-[2px] after:bg-[#001D6C] text-lg text-[#001D6C]">
+					Sign In
+				</Link>
+			)}
+			{!isAuthenticated && (
+				<Link
+					href="/signup"
+					className="focus:outline-none focus:ring-2 focus:ring-white font-medium relative after:absolute [&:not(:hover)]:after:hidden after:top-11/10 after:left-0 after:w-full after:h-[2px] after:bg-[#001D6C] text-lg text-[#001D6C]">
+					Sign Up
+				</Link>
+			)}
+			{isAuthenticated && (
+				<Link
+					href="/profile"
+					className="focus:outline-none focus:ring-2 focus:ring-white font-medium relative after:absolute [&:not(:hover)]:after:hidden after:top-11/10 after:left-0 after:w-full after:h-[2px] after:bg-[#001D6C] text-lg text-[#001D6C]">
+					Profile
+				</Link>
+			)}
+			{isAuthenticated && (
+				<button
+					onClick={() => logout()}
+					className="focus:outline-none focus:ring-2 focus:ring-white font-medium cursor-pointer relative after:absolute [&:not(:hover)]:after:hidden after:top-11/10 after:left-0 after:w-full after:h-[2px] after:bg-[#001D6C] text-lg text-[#001D6C]">
+					Logout
+				</button>
+			)}
+			<Link
+				href={isAuthenticated ? "/new-textbook" : "/login"}
+				className="px-8 py-3 bg-rose-800 text-white rounded-xl font-(family-name:--font-poppins) font-light text-nowrap">
+				Sell a book
+			</Link>
+		</header>
+	)
 }
 
 export default Navbar
